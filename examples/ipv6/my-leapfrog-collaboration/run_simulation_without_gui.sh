8cc98b7b152644d8fc1307b7dc41c61cf7fe13d0
Tadanori MATSUI <tmatsui@pc-d16-04.rennes.enst-bretagne.fr>
2016-07-06 14:12:08 +0200

    modify script file

--- examples/ipv6/my-leapfrog-collaboration/run_simulation_without_gui.sh
+++ examples/ipv6/my-leapfrog-collaboration/run_simulation_without_gui.sh
@@ -2,13 +2,17 @@
 
 cd result
 
-for i in `seq 10`
+for i in `seq 3`
 do
   if [ -d $i ]; then
+    echo dir already exist. remove $i
     rm -rf $i
   fi
+  echo make directory $i
   mkdir $i
+  echo entering $i
   cd $i
-  java -jar /home/tada/contiki/tools/cooja/dist/cooja.jar -nogui=/home/tada/contiki/examples/ipv6/my-leapfrog-collaboration/scenario/ladder-sender-replicate.csc -contiki=/home/tada/contiki/
+  echo execute simulation
+  java -jar /homes/tmatsui/contiki/tools/cooja/dist/cooja.jar -nogui=/homes/tmatsui/contiki/examples/ipv6/my-leapfrog-collaboration/scenario/ladder-sender-replicate.csc -contiki=/homes/tmatsui/contiki/
   cd ..
 done
--- examples/ipv6/my-leapfrog-collaboration/scenario/script.js
+++ examples/ipv6/my-leapfrog-collaboration/scenario/script.js
@@ -9,15 +9,17 @@
 
 send_count = 0;
 receive_count = 0;
+replication_count = 0;
+elimination_count = 0;
 
-TIMEOUT(180000, log.log("Simulation time expired PDR " + (receive_count / send_count) + " % #send " + send_count +  " #receive " + receive_count + "\n")); /* milliseconds. print last msg at timeout */
+TIMEOUT(3600000, log.log("Simulation time expired Time " + time + " PDR " + (receive_count / send_count) + " % #send " + send_count +  " #receive " + receive_count + " #replication " + replication_count + " #elimination " + elimination_count +  "\n")); /* milliseconds. print last msg at timeout */
 
 log.log("Simulation starts\n");
 
 while(true){
     YIELD(); /* wait for another mote output */
     if(msg.match(/Hello/)){ //if "msg" contains "Hello" in it, write "msg" to log
-      log.log("COM: ID: " + id + " " + msg + "\n");
+      log.log("COM: ID: " + id + " time " + time + " " + msg + "\n");
       if(msg.match(/Sending/)){ //count up tx
         send_count++;
         //log.log(send_count + "\n");
@@ -25,7 +27,13 @@ while(true){
         receive_count++;
         //log.log(receive_count + "\n");
       }
-    }else if(msg.match(/radio/)){
-      log.log("PWR: ID: " + id + " " + msg + "\n");
+    }else if(msg.match(/^(?=.*P)(?=.*radio)/)){
+      log.log("PWR: ID: " + id + " time " + time + " " + msg + "\n");
+    }else if(msg.match(/Starting/)){
+      log.log("Start: ID: " + id + " time " + time + " " + msg + "\n");
+    }else if(msg.match(/Replication/)){
+      replication_count++;
+    }else if(msg.match(/Elimination/)){
+      elimination_count++;
     }
 }
