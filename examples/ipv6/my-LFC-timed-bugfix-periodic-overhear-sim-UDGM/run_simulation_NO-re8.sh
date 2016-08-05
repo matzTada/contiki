9337f467b857c75ed4ea4907ce783e3a01eba827
TadaMatz <matz@west.sd.keio.ac.jp>
2016-08-05 18:08:45 +0200

    prepare for UDGM simulation

---
+++ examples/ipv6/my-LFC-timed-bugfix-periodic-overhear-sim-UDGM/run_simulation_NO-re8.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+
+
+for j in 100 90 80 70 60 50 40 30 20 10
+do
+  echo entering
+  cd scenario
+  pwd
+  echo re-write csc file
+  python xml_re_writer.py overhearing_udgm_NO-re8.csc 100 $j
+  echo return tp upper directory
+  cd ..
+  pwd
+
+  echo entering
+  cd ./result/normal-re8
+  pwd
+
+  if [ -d "100-${j}" ]; then
+    echo dir already exist. 
+  else
+    echo make directory 100-${j}
+    mkdir "100-${j}"
+  fi
+  echo entering
+  cd "100-${j}"
+  pwd
+
+  for i in 1 2 3 4 5 6 7 8 9 10
+  do
+    if [ -d $i ]; then
+      echo dir already exist. remove $i
+      rm -rf $i
+    fi
+    echo make directory $i
+    mkdir $i
+    echo entering $i
+    cd $i
+    pwd
+    echo execute simulation
+    java -jar $HOME/contiki/tools/cooja/dist/cooja.jar -nogui=$HOME/contiki/examples/ipv6/my-LFC-timed-bugfix-periodic-overhear-sim-UDGM/scenario/overhearing_udgm_NO-re8.csc -contiki=$HOME/contiki/
+    echo return to upper directory
+    cd ..
+    pwd
+  done
+  echo return to upper directory
+  cd ../../.. 
+  pwd
+done
+echo all simulation finished. good job.
+echo all simulation finished. good job.
+echo all simulation finished. good job.
