f5f5f4f8375cfcec4ce58c4a4338f082dcaa2273
matzTada <matz@west.sd.keio.ac.jp>
2016-09-02 10:19:30 +0200

    confirm Thunder got 100%PDR

--- examples/ipv6/my-LFC-from-old/project-conf.h
+++ examples/ipv6/my-LFC-from-old/project-conf.h
@@ -38,7 +38,8 @@
 //modified by TadaMatz 1/July/2016
 //#define IGNORE_TSCH_RESEND //ignore TSCH resending in tsch-slot-operation
 #define TSCH_CONF_MAC_MAX_FRAME_RETRIES 0//here can controll the number of re-transmission in TSCH
-//#define WITH_LEAPFROG
+
+#define WITH_LEAPFROG
 
 //#define WITH_LEAPFROG_TSCH //if defined, make the new timeslot for alt-parent
 //#define CONDUCT_ORCHESTRA //Scheduling based on Orchestra idea. Each slotframe has different lengths
@@ -47,7 +48,7 @@
 
 #define WITH_THUNDER //do not use Orchestra. Fixed TSCH schedule
 
-//#define WITH_POWERTRACE // for power trace
+#define WITH_POWERTRACE // for power trace
 #define WITH_STABLETIMER // for waiting application traffic until network stabilized
 
 //#define ORCHESTRA_CONF_UNICAST_SENDER_BASED 1
