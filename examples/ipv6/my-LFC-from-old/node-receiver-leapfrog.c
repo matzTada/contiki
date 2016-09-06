1664d583cda0829f152ed71e08a8291983a92169
TadaMatz <matz@west.sd.keio.ac.jp>
2016-09-06 15:46:51 +0200

    disable print network and schedule

--- examples/ipv6/my-LFC-from-old/node-receiver-leapfrog.c
+++ examples/ipv6/my-LFC-from-old/node-receiver-leapfrog.c
@@ -472,9 +472,9 @@ PROCESS_THREAD(node_process, ev, data)
   etimer_set(&et, CLOCK_SECOND * 60);
   //etimer_set(&et, CLOCK_SECOND * 10);
   while(1) {      
-    print_network_status();
+    // print_network_status();
     /*Print tsch schedule*/
-    tsch_schedule_print();
+    // tsch_schedule_print();
     PROCESS_YIELD_UNTIL(etimer_expired(&et));
     etimer_reset(&et);
   }
